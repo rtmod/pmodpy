0ee708d7d07c40802b91fa2edd87bc7e2528ce73
corybrunson <jabrunso@vt.edu>
2017-10-29 09:20:22 -0400

    reproducible bug

--- Possible_bug/Possible_bug.py
+++ Possible_bug/Possible_bug.py
@@ -1,14 +1,15 @@
 import cvxpy,numpy
 
-CONSTRAINTS = numpy.load("matrix_of_const.npy")
-x= cvxpy.Variable(26)
-list_of_constraints=[x>=0]
+CONSTRAINTS = numpy.loadtxt('constraints.txt', delimiter = ',')
 
-obj = cvxpy.Minimize(cvxpy.pnorm(x,1))
+x = cvxpy.Variable(26)
+list_of_constraints = [x >= 0]
+
+obj = cvxpy.Minimize(cvxpy.pnorm(x, 1))
 
 for i in range(len(CONSTRAINTS)):
-    list_of_constraints.append(1 <= numpy.transpose(CONSTRAINTS[i])*x)
+    list_of_constraints.append(1 <= numpy.transpose(CONSTRAINTS[i]) * x)
 
 prob = cvxpy.Problem(obj, list_of_constraints)   
-y=prob.solve()
+y = prob.solve()
 print(x.value)
