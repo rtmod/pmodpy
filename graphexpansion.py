67319fdf2df487dc137dcfa8ffe63588631585b9
Luis Sordo Vieira <l.sordovieira@gmail.com>
2017-11-04 08:24:17 -0400

    Python experimental

--- graphexpansion.py
+++ graphexpansion.py
@@ -1,20 +1,22 @@
 
+
+
+
 from igraph import *
 
 def expand_graph(graph):
-    for i in range(1,len(g.es())+1):
-        x=g.es.select("syn_group"==i)
+    for i in range(1,len(graph.es())+1):
+        x=graph.es.select(syngroup_eq=i)
         
         if len(x)==0:
             break
         else:
             target=x[0].target
-            comp=graph.add_vertex("comp")
+            comp=graph.add_vertex(name="c"+str(i))
             for j in x:
                 graph.add_edge(j.source,comp)
-                
-            graph.add_edge(comp,target)
             graph.delete_edges(x)
+            graph.add_edge(comp,target)
     return graph
             
             
