f3be065b725371664e3d33dc40b5d58737d4ad34
Luis Sordo Vieira <luis.sordovieira@jax.org>
2019-05-26 17:14:44 -0400

    Fixed the ordering on the test_modulus_walks_full_routers example

--- pmodpy/test/test_modwalks.py
+++ pmodpy/test/test_modwalks.py
@@ -35,15 +35,15 @@ def test_modulus_walks_full_routers():
     mod_report = 0.741024
     rho_mod_report = [
         0.296, 0.408, 0.295,
-        0.144, 0.176, 0.232, 0.152, 0.142, 0.153,
-        0.144, 0.139, 0.154, 0.060, 0.142, 0.132, 0.173,
-        0.320, 0.125, 0.014, 0.214, 0.173,
+        0.144, 0.152,  0.176, 0.232, 0.142, 0.153,
+        0.144, 0.139, 0.154, 0.06, 0.142, 0.132, 0.173,
+        0.32, 0.125, 0.014, 0.214, 0.173,
         0.445, 0.555
     ]
-    routers_mod = modwalks.modulus_walks_full(routers, 0, 14, p=2)
-    assert abs(routers_mod[0] - mod_report) < 1e-4
-    assert abs(routers_mod[1] - mod_report) < 1e-4
-    assert max(abs(routers_mod[2] / routers_mod[0] - rho_mod_report)) < 1e-4
+    routers_mod = modwalks.modulus_walks_full(routers, 0, 14, p=2,eps=1e-15)
+    assert abs(routers_mod[0] - mod_report) < 1e-3
+    assert abs(routers_mod[1] - mod_report) < 1e-3
+    assert max(abs(routers_mod[2] / routers_mod[0] - rho_mod_report)) < 1e-3
 
 
 def test_modulus_walks_density_shakeri1a():
@@ -56,6 +56,12 @@ def test_modulus_walks_density_shakeri1b():
     shakeri1b = examplegraphs.Shakeri_1b()
     shakeri1b_mod = modwalks.modulus_walks_density(shakeri1b, 0, 8, p=2)
     assert abs(shakeri1b_mod[0] - 0.5) < 1e-4
+
+
+def test_modulus_walks_density_shakeri1c():
+    shakeri1c = examplegraphs.Shakeri_1c()
+    shakeri1c_mod = modwalks.modulus_walks_density(shakeri1c, 0, 8, p=2)
+    assert abs(shakeri1c_mod[0] - 0.5161) < 1e-4
 
 
 def test_modulus_walks_density_shakeri1d():
