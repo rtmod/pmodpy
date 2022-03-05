19a47bc7b4eb453a56d08295f916dc15381b4639
Igor Sokolov <igor.sokolov@ufl.edu>
2022-03-05 16:41:19 -0500

    WSA example graphs added

--- sandbox/igraph_graph.py
+++ sandbox/igraph_graph.py
@@ -1,14 +1,11 @@
-from igraph import Graph
+from igraph import *
+
+# example graphs from Wang et al. 2013
+dag = Graph(directed = True) # directed acyclic graph, no composite nodes
+dag.add_vertices(10)
+dag.add_edges([(0,1), (0,2), (0,3), (1,4), (2,5), (1,5), (3,2), (3,6), (4,7), (5,7), (5,9), (7,9), (6,8), (6,9), (8,9)])
 
 
-
-line = Graph(directed=True)
-line.add_vertices(3)
-line.add_edges([(0, 1), (1, 2)])
-print(line)
-
-
-graph = Graph(directed = True)
-graph.add_vertices(5)
-graph.add_edges([(0,1), (0,2), (2,1), (1,3), (3,2), (1,4), (3,4)])
-print(graph)
+dcg = Graph(directed = True) # directed cyclic graph, no composite nodes
+dcg.add_vertices(8)
+dcg.add_edges([(0,1), (0,2), (0,3), (3,4), (4,2), (2,5), (1,5), (5,4), (4,6), (6,5), (5,7), (6,7)])
